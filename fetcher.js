3cb49f86af3db57c372d968627bc3f6b64be9599
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-07-29 21:05:12 +0000

    fetcher.js and related files created

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const request = require('request');
+let size = 0;
+const fetcher = (url, path) => {
+  request(url, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    size = body.length;
+    writer(body, path);
+  });
+  
+};
+
+const writer = (content, path) => {
+  fs.writeFile(path, content, err => {
+    if (err) {
+      console.log(err);
+      return
+    }
+  });
+  message(content, path);
+};
+const message = (content, path) => {
+  let size = content.length;
+  console.log(`Downloaded and saved ${size} to ${path}`);
+};
+fetcher(process.argv[2], process.argv[3]);
+
+
